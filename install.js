1ccf44d6aa4a991262d4e444bdf1c848777aca72
6Morpheus6 <wanton-decay@gmx.net>
2025-09-13 19:26:29 +0200

    use uv, call torch.js, remove fs.share

--- install.js
+++ install.js
@@ -1,66 +1,71 @@
-const config = require("./config.js")
-const pre = require("./pre.js")
-module.exports = async (kernel) => {
-  let script = {
-    run: [{
-      // differentialdiffusion
-      method: "shell.run",
-      params: {
-        message: [
-          "git clone https://huggingface.co/spaces/cocktailpeanut/differential-diffusion app",
-        ]
-      }
-    }, {
-      method: "shell.run",
+module.exports = {
+  run: [{
+    // differentialdiffusion
+    method: "shell.run",
+    params: {
+      message: [
+        "git clone https://huggingface.co/spaces/cocktailpeanut/differential-diffusion app",
+      ]
+    }
+  }, {
+    when: "{{gpu === 'nvidia'}}",
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: "uv pip install -r requirements.txt",
+    }
+  }, {
+    when: "{{gpu !== 'nvidia'}}",
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: "uv pip install -r requirements-mps.txt",
+    }
+  }, {
+    // depthanything
+    method: "shell.run",
+    params: {
+      message: [
+        "git clone https://github.com/peanutcocktail/Depth-Anything depth_anything"
+      ],
+      path: "app"
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "../env",
+      path: "app/depth_anything",
+      message: [
+        "uv pip install -r requirements.txt"
+      ],
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "app/env",
+      message: [
+        "uv pip install -r depth-requirements.txt",
+        "uv pip install transformers"
+      ],
+    }
+  }, {
+    method: "script.start",
+    params: {
+      uri: "torch.js",
       params: {
         venv: "env",
         path: "app",
-        message: [
-          (kernel.gpu === 'nvidia' ?  "pip install -r requirements.txt" : 'pip install -r requirements-mps.txt')
-        ],
+        // xformers: true   // uncomment this line if your project requires xformers
+        // triton: true   // uncomment this line if your project requires triton
+        // sageattention: true   // uncomment this line if your project requires sageattention
       }
-    }, {
-      // depthanything
-      method: "shell.run",
-      params: {
-        message: [
-          "git clone https://github.com/peanutcocktail/Depth-Anything depth_anything"
-        ],
-        path: "app"
-      }
-    }, {
-      method: "shell.run",
-      params: {
-        venv: "../env",
-        path: "app/depth_anything",
-        message: [
-          "pip install -r requirements.txt"
-        ],
-      }
-    }, {
-      method: "shell.run",
-      params: {
-        venv: "app/env",
-        message: [
-          "pip install -r depth-requirements.txt",
-          "pip install transformers"
-        ],
-      }
-    }, {
-      method: "fs.share",
-      params: {
-        venv: "app/env"
-      }
-    }, {
-      method: "notify",
-      params: {
-        html: "Click the 'start' tab to get started!"
-      }
-    }]
-  }
-  let pre_command = pre(config, kernel)
-  if (pre_command) {
-    script.run[1].params.message = [pre_command].concat(script.run[1].params.message)
-  }
-  return script
+    }
+  }, {
+    method: "notify",
+    params: {
+      html: "Click the 'start' tab to get started!"
+    }
+  }]
 }
